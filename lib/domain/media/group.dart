8ebfab890f87f803c08f8c616cceff8941c455d6
Stefan Vasic <stefan_vasic@outlook.com>
2023-07-31 01:32:22 +0200

    Youtube extended media group added

--- lib/domain/media/group.dart
+++ lib/domain/media/group.dart
@@ -1,7 +1,10 @@
 import 'package:webfeed_advance/domain/media/category.dart';
 import 'package:webfeed_advance/domain/media/content.dart';
 import 'package:webfeed_advance/domain/media/credit.dart';
+import 'package:webfeed_advance/domain/media/description.dart';
 import 'package:webfeed_advance/domain/media/rating.dart';
+import 'package:webfeed_advance/domain/media/thumbnail.dart';
+import 'package:webfeed_advance/domain/media/title.dart';
 import 'package:webfeed_advance/util/iterable.dart';
 import 'package:xml/xml.dart';
 
@@ -10,12 +13,18 @@ class Group {
   final List<Credit>? credits;
   final Category? category;
   final Rating? rating;
+  final Thumbnail? thumbnail;
+  final Title? title;
+  final Description? description;
 
   Group({
     this.contents,
     this.credits,
     this.category,
     this.rating,
+    this.thumbnail,
+    this.title,
+    this.description,
   });
 
   factory Group.parse(XmlElement element) {
@@ -35,6 +44,18 @@ class Group {
       rating: element
           .findElements('media:rating')
           .map((e) => Rating.parse(e))
+          .firstOrNull,
+      thumbnail: element
+          .findElements('media:thumbnail')
+          .map((e) => Thumbnail.parse(e))
+          .firstOrNull,
+      title: element
+          .findElements('media:title')
+          .map((e) => Title.parse(e))
+          .firstOrNull,
+      description: element
+          .findElements('media:title')
+          .map((e) => Description.parse(e))
           .firstOrNull,
     );
   }
